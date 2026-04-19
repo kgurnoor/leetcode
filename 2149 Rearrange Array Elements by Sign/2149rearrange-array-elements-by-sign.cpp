096650fd55bd819e7f8bd65f91a07d7a1b44e2ef
kgurnoor <gurnoorkaur0349@gmail.com>
2026-04-19 16:36:24 +0000

    Code challenge submission for question: 2149 Rearrange Array Elements by Sign

---
+++ 2149 Rearrange Array Elements by Sign/2149rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector <int> ans (nums.size(), 0);
+        int even = 0;
+        int odd = 1;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] > 0){
+                ans[even] = nums[i];
+                even += 2;
+            }
+            if(nums[i] < 0){
+                ans[odd] = nums[i];
+                odd +=2;
+            }
+        }
+        return ans;
+    }
+};
