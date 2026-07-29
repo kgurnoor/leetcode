7084ac6ef5b87688f3d0687fac99bd3a8ec6dea3
Gurnoor Kaur <85249027+kgurnoor@users.noreply.github.com>
2026-07-29 19:45:21 +0530

    Time: 4 ms (81.51%), Space: 12.7 MB (84.33%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
     int calculate(string s) {
-        int n = s.size();
-        char op = '+';
+        s += '+';
+
+        int result = 0;
+        int lastNumber = 0;
         int number = 0;
-        stack<int> st;
-        s += '+'; //Appending '+' ensures the last number gets processed.
-        for(int i = 0; i < n+1; i++){
-            if(s[i] == ' '){
-                continue; //empty space
-            } 
-            if(isdigit(s[i])){ //if digit is encountered in string
-                number = number*10 + (s[i] - '0');
+        char op = '+';
+
+        for (char c : s) {
+
+            if (c == ' ')
                 continue;
-            }if (op == '+'){ //delay the result
-                st.push(number); 
-            }else if(op == '-'){
-                st.push(-1*number);
-            }else if(op == '*'){
-                int top = st.top();
-                st.pop();
-                st.push(top*number); //calculate result immediately
-            }else if(op == '/'){
-                int top = st.top();
-                st.pop();
-                st.push(top/number);
+
+            if (isdigit(c)) {
+                number = number * 10 + (c - '0');
+                continue;
             }
-            op = s[i]; //saving last operator
+
+            if (op == '+') {
+                result += lastNumber;
+                lastNumber = number;
+            }
+            else if (op == '-') {
+                result += lastNumber;
+                lastNumber = -number;
+            }
+            else if (op == '*') {
+                lastNumber *= number;
+            }
+            else if (op == '/') {
+                lastNumber /= number;
+            }
+
+            op = c;
             number = 0;
         }
-        int result= 0;
-        while(!st.empty()){
-            result += st.top();
-            st.pop();
-        }
-        return result;
+
+        return result + lastNumber;
     }
 };
