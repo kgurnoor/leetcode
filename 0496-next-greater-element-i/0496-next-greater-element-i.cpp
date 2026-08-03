dfcd3c95facebf1305add4899530bf0bb01ea9da
Gurnoor Kaur <85249027+kgurnoor@users.noreply.github.com>
2026-08-03 15:39:44 +0530

    Time: 3 ms (40.66%), Space: 12.8 MB (40.22%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
-    vector<int> nextGreater(vector<int>& nums2){
-        int n = nums2.size();
-        vector<int> nge(n,0);
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map <int, int> hashmap; //nums2 element -> nge 
         stack<int> st;
-        for(int i = n-1; i >= 0; i--){
+        vector<int> ans;
+        for(int i = nums2.size()-1; i >= 0; i--){
             while(!st.empty() && st.top() <= nums2[i]){
                 st.pop();
             }
-            if(st.empty()){
-                nge[i] = -1;
-            }else{
-                nge[i] = st.top();
-            }
+            hashmap[nums2[i]] = st.empty()? -1 : st.top();
             st.push(nums2[i]);
         }
-        return nge;    
-    }
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector <int> ngefornums2 = nextGreater(nums2);
-        vector <int> ans(nums1.size());
         for(int i = 0; i < nums1.size(); i++){
-            for(int j = 0;j < nums2.size(); j++){
-                if(nums1[i] == nums2[j]){
-                    ans[i] = ngefornums2[j];
-                    break; //Once you've found the element in nums2, there's no need to continue searching.
-                }
-            }
+            ans.push_back(hashmap[nums1[i]]);
         }
         return ans;
     }
