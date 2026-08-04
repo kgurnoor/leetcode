313d70935de3d608346d2be556926267a4324f63
Gurnoor Kaur <85249027+kgurnoor@users.noreply.github.com>
2026-08-04 13:16:31 +0530

    Time: 3 ms (49.33%), Space: 33.6 MB (28.39%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-        int current = nums[0]; //smalest
+        //returns an iterator pointing to minimum. 
+        int mn = *min_element(nums.begin(), nums.end());
+        //The * dereferences the iterator, giving the actual value.
+        int mx = *max_element(nums.begin(), nums.end());
         vector<int> ans;
-        for(int i = 0; i < n; i++){
-            while(nums[i] > current){
-                ans.push_back(current);
-                current++;
-            } 
-            current = current + 1;
+        unordered_set<int> s(nums.begin(), nums.end());
+         //the set contains 
+            // {
+            // 1,
+            // 4,
+            // 5,
+            // 7
+            // }
+        //Searching in an unordered_set: takes 0(1) time
+        //s.count(6) -> returns 1 if exists and 0 if it doesnt
+        for(int i = mn+1 ; i < mx; i++){ //between mn and mx
+            if(!s.count(i)){
+                ans.push_back(i);
+            }
         }
         return ans;
     }
