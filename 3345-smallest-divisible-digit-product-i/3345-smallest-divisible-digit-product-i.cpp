2e45ec14c9b1ed53dc332d70eea90e261f3f48c0
Gurnoor Kaur <85249027+kgurnoor@users.noreply.github.com>
2026-08-06 23:04:11 +0530

    Time: 0 ms (100%), Space: 8.5 MB (97.62%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     int smallestNumber(int n, int t) {
-        auto [q, r] = div(n, 10);
-
-        int req = t / gcd(q + (10 - q) / 10, t);
-        int nxt = ((r + req - 1) / req) * req;
-        int x = nxt - (nxt - 10) * (nxt / 10);
-
-        return q * 10 + x;
+        while(true){
+            int product = 1;
+            int x = n;
+            while(x != 0){
+                product = product * (x % 10);
+                x = x/10;
+            }
+            if(product % t == 0){
+                return n;
+            }
+            n++;
+        }
     }
 };
